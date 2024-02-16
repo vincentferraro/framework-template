29924796209b375ab2fe9f05b79c216e9b6ae255
vincentferraro <v.ferraro@live.fr>
2024-02-15 20:34:45 -0500

    add spring project

---
+++ src/main/java/com/framework/frameworktemplate/FrameworkTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.framework.frameworktemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworkTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/framework/frameworktemplate/FrameworkTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.framework.frameworktemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
